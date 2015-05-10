7c3998e875d62c847a4305f702df1e60cf453ab9
Harsh Shah <harshn08@gmail.com>
2015-05-10 12:59:35 -0700

    Update dbConnection.js

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -6,7 +6,7 @@ var pool;
 
 var redis = require("redis");
 var client = redis.createClient(6379,"127.0.0.1");
-//redis-cmpe282.cysnho.0001.usw1.cache.amazonaws.com
+
 
 function getRedisConnection(){
 	return client;
